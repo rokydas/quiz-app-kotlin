40eaac0fe536676abcf243b655d317f8fbc71d1c
rokydas <droky136@gmail.com>
2021-09-16 00:50:38 +0600

    design done

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startBtn.setOnClickListener {
+            if(editName.text.toString() == "") {
+                Toast.makeText(this, "Name cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                intent = Intent(this, QuizActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
